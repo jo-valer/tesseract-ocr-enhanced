2039c29fc386b5e208fbeacbda26ce8509be6651
Giovanni Valer <giovanni.valer@studenti.unitn.it>
2024-01-23 12:11:02 +0100

    Full trackbar mode

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Repo for our SIV Project
 
 ## Installation
 We used Python 3.12.0 and Tesseract-OCR 5.3.3. See requirements.txt for the required packages.
+
+## Src
+The src folder contains the source code for our project.
+There are different functionalities:
+- `manual_trackbar.py`: manually adjust the parameters
+- `auto_trackbar.py`: automatically adjust the parameters
+
+At the moment, .ipynb files are used for testing purposes. They will be removed in the future.
--- src/manual_trackbar.py
+++ src/manual_trackbar.py
@@ -39,7 +39,8 @@ def manual_trackbar_ocr(image):
     if DEBUG1:
         print("DEBUG: manual_trackbar_ocr()")
         text = pytesseract.image_to_string(image, lang='eng')
-        print(text)
+        # print text in red in console
+        print("\033[91m {}\033[00m" .format(text))
 
     else:
         cv2.namedWindow('image')
@@ -60,11 +61,7 @@ def manual_trackbar_ocr(image):
         us = 255
         uv = 255
 
-        # cv2.imshow('image', image)
-
-        # create full white image with same width of the original image and height of 20 pixels
-        white_image = np.zeros((20, image.shape[1], 3), np.uint8)
-        cv2.imshow('image', np.hstack([image, white_image]))
+        cv2.imshow('image', image)
 
         while(1):            
             # get current positions of six trackbars
@@ -94,27 +91,21 @@ def manual_trackbar_ocr(image):
             # Bitwise-AND mask and original image
             res = cv2.bitwise_and(image, image, mask=mask)
 
-            # Display the resulting frame
-            # cv2.imshow('image', res)
-            # k = cv2.waitKey(1) & 0xFF
-            # if k == 27: # wait for ESC key to exit
-            #     break
-
             # OCR
             if (lh_i != lh) or (ls_i != ls) or (lv_i != lv) or (uh_i != uh) or (us_i != us) or (uv_i != uv):
                 text = pytesseract.image_to_string(res, lang='eng')
                 # print text in cv2 window
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(res, text, (10, 500), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
-                # cv2.imshow('image', res)
-                cv2.imshow('image', np.hstack([res, white_image]))
+                # font = cv2.FONT_HERSHEY_SIMPLEX
+                # cv2.putText(res, text, (10, 500), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.imshow('image', res)
+                
+                # print text in green in console
+                print("\033[92m {}\033[00m" .format(text))  
 
             k = cv2.waitKey(1) & 0xFF
             if k == 27: # wait for ESC key to exit
                 break
                 
-
-        # When everything done, release the capture
         cv2.destroyAllWindows()
 
 
@@ -122,7 +113,8 @@ if __name__ == "__main__":
     #DEBUG
     PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath("FILEPATH")))
     image_path = os.path.join(PARENT_DIR, "images", "001.png")
-    print(pytesseract.image_to_string(Image.open(image_path)))
+    # print text in red in console
+    print("\033[91m {}\033[00m" .format(pytesseract.image_to_string(Image.open(image_path))))
 
     # Parent directory path
     PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,8 +128,6 @@ if __name__ == "__main__":
         ap.add_argument("-i", "--image", required=True, help="Path to the image")
         args = vars(ap.parse_args())
 
-        # Load the image
         image = cv2.imread(args["image"])
 
-    # Call the function
     manual_trackbar_ocr(image)
